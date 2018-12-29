c4d067f1cc3175f95aceedfdae82c0cb3b71a0b6
TakaakiFuruse <takaakifuruse@gmail.com>
2018-12-29 14:36:53 +0900

    com

--- types_traits_lifetimes/src/lib.rs
+++ types_traits_lifetimes/src/lib.rs
@@ -1,5 +1,13 @@
 pub trait Summary {
     fn summarize(&self) -> String;
+    fn article_type(&self) -> String;
+
+    fn read_more(&self) -> String {
+        "Read more.....".to_string()
+    }
+    fn paywall(&self) -> String {
+        format!("Pay us if you wanna read this {}...", self.article_type())
+    }
 }
 
 pub struct NewsArticle {
@@ -10,12 +18,22 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
+    fn article_type(&self) -> String {
+        "News Article".to_string()
+    }
     fn summarize(&self) -> String {
         format!(
             "headline: {}, author: {}, location: {}",
             self.headline, self.author, self.location
         )
     }
+}
+
+pub struct BlogArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
 }
 
 pub struct Tweet {
@@ -26,7 +44,64 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
+    fn article_type(&self) -> String {
+        "Tweet".to_string()
+    }
+
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
+    }
+}
+
+pub trait SayIt {
+    fn the_call(&self) -> String;
+}
+
+pub struct Mituo {
+    pub shout: String,
+}
+
+impl SayIt for Mituo {
+    fn the_call(&self) -> String {
+        self.shout.to_string()
+    }
+}
+
+pub struct Senda {
+    pub shout: String,
+}
+
+impl SayIt for Senda {
+    fn the_call(&self) -> String {
+        self.shout.to_string()
+    }
+}
+
+pub struct NahaNaha {
+    pub shout: String,
+}
+
+impl NahaNaha {
+    pub fn the_call(&self) -> String {
+        self.shout.to_string()
+    }
+}
+
+pub struct ShoutOut<T> {
+    pub obj: T,
+}
+
+impl<T> ShoutOut<T> {
+    pub fn kimoi(&self) -> String {
+        "デュフフフフ".to_string()
+    }
+}
+
+impl<T> ShoutOut<T>
+where
+    T: SayIt,
+{
+    pub fn shoutout(&self) -> String {
+        "せんだ、みつお、なはなは".to_string()
     }
 }
--- types_traits_lifetimes/src/main.rs
+++ types_traits_lifetimes/src/main.rs
@@ -1,5 +1,13 @@
+#![allow(dead_code)]
+
+use types_traits_lifetimes::Mituo;
+use types_traits_lifetimes::NahaNaha;
 use types_traits_lifetimes::NewsArticle;
+use types_traits_lifetimes::SayIt;
+use types_traits_lifetimes::Senda;
+use types_traits_lifetimes::ShoutOut;
 use types_traits_lifetimes::Summary;
+use types_traits_lifetimes::Tweet;
 
 fn largest_i32(list: &[i32]) -> i32 {
     let mut largest = list[0];
@@ -25,16 +33,33 @@ fn largest_char(list: &[char]) -> char {
     largest_char
 }
 
-// これでは動かない
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item
-//         }
-//     }
-//     largest
-// }
+// PartialOrd + Copy なTraitだけ扱うようにする。
+fn largest<T>(list: &[T]) -> T
+where
+    T: PartialOrd + Copy,
+{
+    let mut largest = list[0];
+    for item in list.iter() {
+        if item > &largest {
+            largest = *item
+        }
+    }
+    largest
+}
+
+// return a reference to a T value in the slice
+fn largest2<T>(list: &[T]) -> &T
+where
+    T: PartialOrd,
+{
+    let mut largest = &list[0];
+    for item in list.iter() {
+        if item > largest {
+            largest = &item
+        }
+    }
+    largest
+}
 
 #[derive(Debug)]
 struct Point1<T> {
@@ -80,9 +105,12 @@ fn main() {
     println!("largest num: {}", result);
 
     let char_list = vec!['y', 'm', 's', 'q'];
-    let result = largest_char(&char_list);
+    let result1 = largest_char(&char_list);
 
-    println!("largest char {}", result);
+    println!("largest char {}", result1);
+
+    println!("{}", largest(&number_list));
+    println!("{}", largest2(&number_list));
 
     // OK
     println!("{:?}", Point1 { x: 1, y: 2 });
@@ -108,4 +136,88 @@ fn main() {
     };
 
     println!("{}", news.summarize());
+    println!("{}", news.read_more());
+    println!("{}", news.paywall());
+
+    let tweet = Tweet {
+        username: "ME".to_string(),
+        content: "super important".to_string(),
+        reply: true,
+        retweet: false,
+    };
+
+    println!("{}", tweet.summarize());
+    println!("{}", tweet.read_more());
+    println!("{}", tweet.paywall());
+
+    // 下と同じ結果になる
+    notify(&tweet);
+    notify(&news);
+
+    // 上と同じ結果になる
+    // notify_verbose(&tweet);
+    // notify_verbose(&news)
+
+    // Using Trait Bounds to Conditionally Implement Methods
+    let senda = Senda {
+        shout: "せんだ".to_string(),
+    };
+    let mituo = Mituo {
+        shout: "みつお".to_string(),
+    };
+    let nahanaha = NahaNaha {
+        shout: "なはなは".to_string(),
+    };
+
+    // ひとつだけSayItがない状態
+    println!(
+        "{}, {}, {}",
+        senda.the_call(),
+        mituo.the_call(),
+        nahanaha.the_call()
+    );
+
+    let shout1 = ShoutOut { obj: senda };
+    let shout2 = ShoutOut { obj: mituo };
+    let shout3 = ShoutOut { obj: nahanaha };
+
+    // kimoi は全部あるが
+    println!("{}, {}, {}", shout1.kimoi(), shout2.kimoi(), shout3.kimoi());
+
+    // shoutout は shout3 にはない
+    println!("{}, {}", shout1.shoutout(), shout2.shoutout(),);
+
+    // 下をやるとエラー。タイプがついてこないので、メソッドがない。
+    // println!("{}", shout3.shoutout());
+    // = note: the method `shoutout` exists but the following trait bounds were not satisfied:
+    // `types_traits_lifetimes::NahaNaha : types_traits_lifetimes::SayIt`
 }
+
+// traits as arguments (syntax sugar for verbose one)
+pub fn notify(item: &impl Summary) {
+    println!("New {} !!", item.article_type());
+}
+
+// trait bounds
+pub fn notify_verbose<T: Summary>(item: T) {
+    println!("Breaking new {}!!", item.article_type());
+}
+
+// traits as arguments (2つ書くのはめんどい)
+// pub fn notify2(item1: impl Summary, item2: imple Summary) {
+//     println!("New {} {}!!", item1.article_type(), item2.article_type());
+// }
+
+// trait bounds (こっちのほうが簡潔)
+// pub fn notify_verbose2<T: Summary>(item1: T, item2: T) {
+//     println!("Breaking new {} {}!!", item1.article_type(), item2.article_type());
+// }
+
+// where clause (defining multiple traits in pretty way)
+// fn some_function<T: Display + Clone, U: Clone + Debug>(t: T, u: U) -> i32 {
+
+// Expressing bounds with a `where` clause
+// fn some_function<T, U>(t: T, u: U) -> i32
+// where T: Display + Clone,
+//       U: Clone + Debug
+// {}
