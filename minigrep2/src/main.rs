01bef126e762f70171a775696c81c671668ca69f
TakaakiFuruse <takaakifuruse@gmail.com>
2019-03-17 16:29:58 +0900

    fixed minigrep 2

--- minigrep2/src/main.rs
+++ minigrep2/src/main.rs
@@ -1,4 +1,4 @@
-use minigrep2::run;
+use minigrep2;
 use minigrep2::Config;
 use std::env;
 use std::process;
@@ -6,7 +6,11 @@ fn main() {
     let args: &Vec<String> = &env::args().collect();
     let config = Config::new(args).unwrap_or_else(|err| {
         print!("Config parse error: {}", err);
-        process::exit(1)
+        process::exit(1);
     });
-    run(config);
+
+    if let Err(e) = minigrep2::run(config) {
+        println!("App error {}", e);
+        process::exit(1);
+    };
 }
