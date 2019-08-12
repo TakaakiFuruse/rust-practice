45739e49a6087a64a15f0952a70ffd517b93bd4f
TakaakiFuruse <takaakifuruse@gmail.com>
2019-08-12 11:17:00 +0900

    derive mode doen, created enum from string

--- snippets/derive_mode/src/main.rs
+++ snippets/derive_mode/src/main.rs
@@ -6,6 +6,14 @@ pub mod name {
         pub myattr1: String,
         pub myattr2: String,
     }
+
+    #[derive(builder)]
+    pub struct YourStruct {
+        pub yourattr1: String,
+        pub yourattr2: String,
+        pub yourattr3: String,
+    }
+
 }
 
 fn main() {
@@ -16,4 +24,14 @@ fn main() {
 
     assert_eq!(ms.myattr1, "aaa".to_string());
     assert_eq!(ms.myattr2, "bbb".to_string());
+
+    let ys = name::YourStructBuilder::new()
+        .with_yourattr1("aaa".to_string())
+        .with_yourattr2("bbb".to_string())
+        .with_yourattr3("ccc".to_string())
+        .build();
+
+    assert_eq!(ys.yourattr1, "aaa".to_string());
+    assert_eq!(ys.yourattr2, "bbb".to_string());
+    assert_eq!(ys.yourattr3, "ccc".to_string());
 }
