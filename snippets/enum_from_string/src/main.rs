289cd1f00586fc6437230b5011b009dc05d31b7a
TakaakiFuruse <takaakifuruse@gmail.com>
2019-08-12 14:34:40 +0900

    com

--- snippets/enum_from_string/src/main.rs
+++ snippets/enum_from_string/src/main.rs
@@ -1,3 +1,19 @@
+pub mod enum_builder {
+    use enum_from_string::order;
+
+    #[derive(Debug, order)]
+    pub enum Gods {
+        Father,
+        Mother,
+        Maiden,
+        Crone,
+        Warrior,
+        Smith,
+        Stranger,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(enum_builder::Gods::Father.order(), 6);
+    assert_eq!(enum_builder::Gods::Stranger.order(), 0);
 }
